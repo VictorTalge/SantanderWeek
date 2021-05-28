82fc5cab6be8b6497e3004b29c05d2912fee3069
Victor Talge <vt.talge@gmail.com>
2021-05-28 18:03:47 -0300

    Atualizado 28/05

---
+++ Spring/SantanderWeek/src/main/java/com/project/SantanderWeek/SantanderWeekApplication.java
@@ -0,0 +1,14 @@
+package com.project.SantanderWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderWeekApplication {
+
+    public static void main(String[] args) {
+
+    	SpringApplication.run(SantanderWeekApplication.class, args);
+    }
+
+}
